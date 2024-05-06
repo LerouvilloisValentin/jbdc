b9f83b7b8cd06cd9a805f9dafa5d9046b22b2d3f
LerouvilloisValentin <lerouvillois.valentin61@hotmail.fr>
2024-05-06 12:58:39 +0200

    tp2 jbdc

--- src/main/java/frdigi2024/App.java
+++ src/main/java/frdigi2024/App.java
@@ -3,11 +3,23 @@ package frdigi2024;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class App {
 
+	private static final String DB_URL;
+	private static final String DB_USER;
+	private static final String DB_PWD;
+	
+	static {
+		ResourceBundle dbConfig = ResourceBundle.getBundle("db");
+		DB_URL = dbConfig.getString("db.url");
+		DB_USER = dbConfig.getString("db.user");
+		DB_PWD = dbConfig.getString("db.pwd");
+	}
+	
 	public static void main(String[] args) throws SQLException {
-		Connection connection = DriverManager.getConnection("jdbc:mysql://bbefrgii0bh6rhcnwmps-mysql.services.clever-cloud.com:3306/bbefrgii0bh6rhcnwmps","u1x0g0xahjzlykyt","");
+		Connection connection = DriverManager.getConnection(DB_URL,DB_USER,	DB_PWD);
 		System.out.println(connection);
 		connection.close();
 	}
